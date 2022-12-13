409b6ce1a62c511e40741ecc01305bc4a1326dc7
jay <jaythoriya8779@gmail.com>
2022-12-13 23:05:06 +0530

    done concept of inheritance and constructer

---
+++ java/Inheritans.java
@@ -0,0 +1,34 @@
+class base {
+    base(){ System.out.println("I am a base class constracter !!!");}
+    base(int a){ System.out.println("I am a base class constracter ::: the value of A is : "+a);}
+}
+
+class derived extends base{
+    derived(){ System.out.println("I am a derived class constracter !!!");}
+    
+    derived(int a,int b){ 
+        
+        super(a);
+
+        System.out.printf("I am a base class constracter ::: the value of A is : %d and value of B is : %d  \n",a,b);}
+}
+
+class chaid extends derived{
+    chaid(){ System.out.println("I am a chaid class constracter !!!");}
+    
+    chaid(int a,int b,int c){ super(a,b); System.out.printf("I achaide class constracter ::: the value of A is : %d and value of B is : %d and value of c is : %d  ",a,b,c);}
+}
+
+public class Inheritans {
+
+    public static void main(String[] args) {
+        
+        // base b = new base(2);
+        
+        // derived d = new derived(7,5);
+
+        chaid c = new chaid(2,3,5);
+        
+    }
+    
+}
