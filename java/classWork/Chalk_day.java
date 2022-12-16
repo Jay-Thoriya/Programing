6c339a29c6e4441b45ad5f73a0a8c4dbb4d5a9e5
jay <jaythoriya8779@gmail.com>
2022-12-16 11:24:34 +0530

    solve chalk program

---
+++ java/classWork/Chalk_day.java
@@ -0,0 +1,16 @@
+package classWork;
+import java.util.*;
+
+public class Chalk_day {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter chalk you have : ");
+        int n  = sc.nextInt();
+        if(n>=4 && n<=666){
+            System.out.println((n+(int)Math.sqrt(n)+1)+" days you have used");
+        }
+        else{ System.out.println("Invalid input...");}
+        sc.close();
+    }
+}
