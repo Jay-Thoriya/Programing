41473a2deb76f2a21dc8515eba2fde4362d29731
jay <jaythoriya8779@gmail.com>
2023-03-02 11:09:15 +0530

    time and date creating  in react

--- react/homeapp/src/index.js
+++ react/homeapp/src/index.js
@@ -3,4 +3,16 @@
 
 import React  from 'react';
 import ReactDOM from 'react-dom';
-ReactDOM.render( <h1> Hello World !! </h1> , document.getElementById('root'))
+const currentDate = new Date().toLocaleDateString();
+const currentTime = new Date().toLocaleTimeString();
+
+const fname = "jay";
+const lname = "Thoriya";
+
+ReactDOM.render( 
+[
+<h1>My frist name is {fname} and last name is {lname} .</h1> ,
+<p>Today Date is {currentDate}</p>,
+<p> Current Time is {currentTime}</p>
+],
+document.getElementById('root'))
