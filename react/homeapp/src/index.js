4b67f2433e62500fa4f9d9c0a1ee6d01d7e13097
jay <jaythoriya8779@gmail.com>
2023-03-03 17:32:07 +0530

    display time and date

--- react/homeapp/src/index.js
+++ react/homeapp/src/index.js
@@ -3,16 +3,54 @@
 
 import React  from 'react';
 import ReactDOM from 'react-dom';
-const currentDate = new Date().toLocaleDateString();
-const currentTime = new Date().toLocaleTimeString();
+import './index.css';
+const currentDate = new Date(2023,3,2,7).toLocaleDateString();
+const currentTime = new Date(2023,3,2,7).toLocaleTimeString();
 
-const fname = "jay";
-const lname = "Thoriya";
+// const fname = "jay";
+// const lname = "Thoriya";
+// const img1 = "https://picsum.photos/250/300"
+// const img2 = "https://picsum.photos/249/300"
+// const img3 = "https://picsum.photos/251/300"
+// const img4 = "https://picsum.photos/248/300"
 
+const night ={
+    backgroundImage: 'url("https://blog.dana-farber.org/insight/wp-content/uploads/2016/09/NIght-Scene-with-Moon.jpg")',
+}
+const noon ={
+    backgroundImage: 'url("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQd4aY3Yk1VaTSOPcCmcwc0DkL-GJf--mvm1w&usqp=CAU")',
+    
+}
+const mornig ={
+    backgroundImage: 'url("https://www.google.com/imgres?imgurl=https%3A%2F%2Fstatic.tnn.in%2Fphoto%2Fmsid-95369526%2Cimgsize-491555%2Cwidth-100%2Cheight-200%2Cresizemode-75%2F95369526.jpg&imgrefurl=https%3A%2F%2Fwww.timesnownews.com%2Flifestyle%2F9-lovely-good-morning-images-and-quotes-for-your-whatsapp-status-article-95369526&tbnid=uNB3bB1Ues2pJM&vet=12ahUKEwjE6sXSsb39AhUzw3MBHQ0MAP0QMygXegUIARCOAg..i&docid=e9V1Z88ruBV4jM&w=1920&h=1080&q=mornigimages&ved=2ahUKEwjE6sXSsb39AhUzw3MBHQ0MAP0QMygXegUIARCOAg")',
+}
+
+let greeting = '';
+function timeing(){ 
+if(currentDate >=1 && currentDate < 12){
+    greeting='Good Mornig';
+    return mornig;
+}
+else if (currentDate >=12 && currentDate < 19){
+    greeting='Good After noon';
+    return noon;
+}
+else{
+    greeting='Good Night';
+    return night;
+}
+
+}
+console.log(timeing());
 ReactDOM.render( 
-[
-<h1>My frist name is {fname} and last name is {lname} .</h1> ,
-<p>Today Date is {currentDate}</p>,
+<div  style = {timeing()
+{/* <h1 className='heading'>My name is {fname} {lname} </h1>  */}
+<p >Hello Sir , {greeting}</p>
+<p>Today Date is {currentDate}</p>
 <p> Current Time is {currentTime}</p>
-],
+{/* <img src={img1} alt="random imag"/>
+<img src={img2} alt="random imag"/>
+<img src={img3} alt="random imag"/>
+<img src={img4} alt="random imag"/> */}
+</div>,
 document.getElementById('root'))
