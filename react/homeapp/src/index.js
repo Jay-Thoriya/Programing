b8c9c48db010f3fcd1a3d8a23f828e1b24c61250
Jay-Thoriya <120302342+Jay-Thoriya@users.noreply.github.com>
2023-03-06 17:31:23 +0530

    making componets

--- react/homeapp/src/index.js
+++ react/homeapp/src/index.js
@@ -1,56 +1,57 @@
 // var React = require('react');
 // var ReactDOM = require('react-dom');
 
-import React  from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
+import name from '/name.jsx'
 import './index.css';
-const currentDate = new Date(2023,3,2,7).toLocaleDateString();
-const currentTime = new Date(2023,3,2,7).toLocaleTimeString();
+const currentDate = new Date(2023, 3, 2, 7).toLocaleDateString();
+const currentTime = new Date(2023, 3, 2, 7).toLocaleTimeString();
 
-// const fname = "jay";
-// const lname = "Thoriya";
-// const img1 = "https://picsum.photos/250/300"
-// const img2 = "https://picsum.photos/249/300"
-// const img3 = "https://picsum.photos/251/300"
-// const img4 = "https://picsum.photos/248/300"
+const fname = "jay";
+const lname = "Thoriya";
+const img1 = "https://picsum.photos/250/300"
+const img2 = "https://picsum.photos/249/300"
+const img3 = "https://picsum.photos/251/300"
+const img4 = "https://picsum.photos/248/300"
 
-const night ={
+const night = {
     backgroundImage: 'url("https://blog.dana-farber.org/insight/wp-content/uploads/2016/09/NIght-Scene-with-Moon.jpg")',
 }
-const noon ={
+const noon = {
     backgroundImage: 'url("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQd4aY3Yk1VaTSOPcCmcwc0DkL-GJf--mvm1w&usqp=CAU")',
-    
+
 }
-const mornig ={
+const mornig = {
     backgroundImage: 'url("https://www.google.com/imgres?imgurl=https%3A%2F%2Fstatic.tnn.in%2Fphoto%2Fmsid-95369526%2Cimgsize-491555%2Cwidth-100%2Cheight-200%2Cresizemode-75%2F95369526.jpg&imgrefurl=https%3A%2F%2Fwww.timesnownews.com%2Flifestyle%2F9-lovely-good-morning-images-and-quotes-for-your-whatsapp-status-article-95369526&tbnid=uNB3bB1Ues2pJM&vet=12ahUKEwjE6sXSsb39AhUzw3MBHQ0MAP0QMygXegUIARCOAg..i&docid=e9V1Z88ruBV4jM&w=1920&h=1080&q=mornigimages&ved=2ahUKEwjE6sXSsb39AhUzw3MBHQ0MAP0QMygXegUIARCOAg")',
 }
 
 let greeting = '';
-function timeing(){ 
-if(currentDate >=1 && currentDate < 12){
-    greeting='Good Mornig';
-    return mornig;
-}
-else if (currentDate >=12 && currentDate < 19){
-    greeting='Good After noon';
-    return noon;
-}
-else{
-    greeting='Good Night';
-    return night;
-}
+function timeing() {
+    if (currentDate >= 1 && currentDate < 12) {
+        greeting = 'Good Mornig';
+        return mornig;
+    }
+    else if (currentDate >= 12 && currentDate < 19) {
+        greeting = 'Good After noon';
+        return noon;
+    }
+    else {
+        greeting = 'Good Night';
+        return night;
+    }
 
 }
 console.log(timeing());
-ReactDOM.render( 
-<div  style = {timeing()
-{/* <h1 className='heading'>My name is {fname} {lname} </h1>  */}
-<p >Hello Sir , {greeting}</p>
-<p>Today Date is {currentDate}</p>
-<p> Current Time is {currentTime}</p>
-{/* <img src={img1} alt="random imag"/>
-<img src={img2} alt="random imag"/>
-<img src={img3} alt="random imag"/>
-<img src={img4} alt="random imag"/> */}
-</div>,
-document.getElementById('root'))
+ReactDOM.render(
+    <div style={{}}>
+        <name/>
+        <p >Hello Sir , {greeting}</p> */
+        <p>Today Date is {currentDate}</p>
+        <p> Current Time is {currentTime}</p>
+        <img src={img1} alt="random imag" />
+        <img src={img2} alt="random imag" />
+        <img src={img3} alt="random imag" />
+        <img src={img4} alt="random imag" />
+    </div>,
+    document.getElementById('root'))
---
+++ react/homeapp/src/name.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const fname = "jay";
+const lname = "Thoriya";
+
+<h1 className='heading'>My name is {fname} {lname} </h1> 
+
