62af7f2b1e134004e8812027a08eee9bc4e0910e
Jay-Thoriya <120302342+Jay-Thoriya@users.noreply.github.com>
2023-03-07 18:13:58 +0530

    making calculater using componats

--- react/homeapp/src/App.js
+++ react/homeapp/src/App.js
@@ -1,46 +1,25 @@
 
-import './App.css';
-
-import './index.css';
-const currentDate = new Date(2023, 3, 2, 7).toLocaleDateString();
-const currentTime = new Date(2023, 3, 2, 7).toLocaleTimeString();
-
-
-const night = {
-    backgroundImage: 'url("https://blog.dana-farber.org/insight/wp-content/uploads/2016/09/NIght-Scene-with-Moon.jpg")',
-}
-const noon = {
-    backgroundImage: 'url("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQd4aY3Yk1VaTSOPcCmcwc0DkL-GJf--mvm1w&usqp=CAU")',
-
-}
-const mornig = {
-    backgroundImage: 'url("https://www.google.com/imgres?imgurl=https%3A%2F%2Fstatic.tnn.in%2Fphoto%2Fmsid-95369526%2Cimgsize-491555%2Cwidth-100%2Cheight-200%2Cresizemode-75%2F95369526.jpg&imgrefurl=https%3A%2F%2Fwww.timesnownews.com%2Flifestyle%2F9-lovely-good-morning-images-and-quotes-for-your-whatsapp-status-article-95369526&tbnid=uNB3bB1Ues2pJM&vet=12ahUKEwjE6sXSsb39AhUzw3MBHQ0MAP0QMygXegUIARCOAg..i&docid=e9V1Z88ruBV4jM&w=1920&h=1080&q=mornigimages&ved=2ahUKEwjE6sXSsb39AhUzw3MBHQ0MAP0QMygXegUIARCOAg")',
-}
-
-let greeting = '';
-function timeing() {
-    if (currentDate >= 1 && currentDate < 12) {
-        greeting = 'Good Mornig';
-        return mornig;
-    }
-    else if (currentDate >= 12 && currentDate < 19) {
-        greeting = 'Good After noon';
-        return noon;
-    }
-    else {
-        greeting = 'Good Night';
-        return night;
-    }
-
-}
-function App() {
+function Sum(x ,y) {
   return (
-    <div style={timeing()}>
-        <p >Hello Sir , {greeting}</p> 
-        <p>Today Date is {currentDate}</p>
-        <p> Current Time is {currentTime}</p>
-    </div>
+    x+y
   );
 }
+function Sub(x ,y) {
+    return (
+      x-y
+    );
+  }
+  function Mal(x ,y) {
+    return (
+      x*y
+    );
+  }
+  function Div(x ,y) {
+    return (
+      x/y
+    );
+  }
 
-export default App;
+export default Sum ;
+export {Sub,Mal,Div}
+
--- react/homeapp/src/index.js
+++ react/homeapp/src/index.js
@@ -3,6 +3,15 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App'
+import * as cal from './App'
 
-ReactDOM.render(<App/> ,document.getElementById('root'))
+ReactDOM.render( 
+    <>
+        <ul>
+            <li> The sum of two number is {cal.default(5,5)}</li>
+            <li> The sub of two number is {cal.Sub(5,5)}</li>
+            <li> The mal of two number is {cal.Mal(5,5)}</li>
+            <li> The div of two number is {cal.Div(10,5)}</li>
+        </ul>
+    </>
+    ,document.getElementById('root'))
