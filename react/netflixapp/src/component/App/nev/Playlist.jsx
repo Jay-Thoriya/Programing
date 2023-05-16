ba6dd03ed05c3f7797d374096fad4821945183ec
Jay-Thoriya <120302342+Jay-Thoriya@users.noreply.github.com>
2023-05-16 16:58:47 +0530

    change main source of data and fixed it playList

--- react/netflixapp/src/component/App/nev/Playlist.jsx
+++ react/netflixapp/src/component/App/nev/Playlist.jsx
@@ -1,56 +1,48 @@
 
 import { useState } from "react";
-import SeriesData from "../card/SeriesData";
 
-function Playlist({setISPlaylist , PlaylistSeriesIds}) {
-    
-    const [ displayText , setDisplayText] = useState("");
-    const [ CheckPlaylistIsOpen , setCheckPlaylistIsOpen] = useState(true)
-
-    function Check_Playlist(){       
-        if(CheckPlaylistIsOpen){
+function Playlist({ setISPlaylist, PlaylistSeriesIds, moviesData }) {
+    const [displayText, setDisplayText] = useState("");
+    const [CheckPlaylistIsOpen, setCheckPlaylistIsOpen] = useState(true)
+    function Check_Playlist() {
+        if (CheckPlaylistIsOpen) {
             OpenPlaylist();
             setCheckPlaylistIsOpen(false)
-        }else{
+        } else {
             ClosePlaylist();
             setCheckPlaylistIsOpen(true)
         }
     }
-    function OpenPlaylist() {   
-
-           //  console.log(PlaylistSeriesIds);
-
-            const result = SeriesData.filter(title =>{
-                for(let i =0 ; i<= PlaylistSeriesIds.length ; i++){
-                    if(title.id === PlaylistSeriesIds[i]){
-                        return true;
-                    }
+    function OpenPlaylist() {
+        const result = moviesData.filter(title => {
+            console.log("title", title)
+            for (let i = 0; i <= PlaylistSeriesIds.length; i++) {
+                if (title.imdbID === PlaylistSeriesIds[i]) {
+                    return true;
                 }
-                return false;
-            });
-           //  console.log("result",result)
-            setDisplayText(<h2 style={{color:"red" , textAlign:"center"}}> Playlist </h2>)
-            setISPlaylist(result)
-
+            }
+            return false;
+        });
+        setDisplayText(<h2 style={{ color: "red", textAlign: "center" }}> Playlist </h2>)
+        setISPlaylist(result)
     }
 
-    function ClosePlaylist(){
-        const result = SeriesData.map(id => id );
-        //console.log(" result is ",result)
+    function ClosePlaylist() {
+        const result = moviesData.map(id => id);
         setISPlaylist(result);
         setDisplayText("");
     }
 
-    
-    return ( 
+
+    return (
         <>
             <button onClick={Check_Playlist} className="PlayListButton">
                 <img src="playlist2.png" className="playlistImg" alt="playlist_img" />
             </button>
-            
+
             {displayText}
-            
-            
+
+
         </>
     );
 }
