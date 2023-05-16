1fdb60a908c3fc053bdc556a442d5ff60db25c39
Jay-Thoriya <120302342+Jay-Thoriya@users.noreply.github.com>
2023-05-16 16:57:49 +0530

    removing all unusual function

--- react/netflixapp/src/component/App/card/SeriesData.jsx
+++ react/netflixapp/src/component/App/card/SeriesData.jsx
@@ -2,11 +2,8 @@ import { useEffect, useState } from "react";
 //"https://imdb-api.com/en/API/Top250Movies/k_dhngf99h" api 250 info
 
 
-function useApi(what, setMovies) {
+function useApi(what, ) {
     const [moviesData, setData] = useState([]);
-    //console.log(moviesData);
-    const [loading, setLoading] = useState(false);
-    const [error, setError] = useState(null);
 
     let API = "http://localhost:3000/" + what;
 
@@ -15,22 +12,18 @@ function useApi(what, setMovies) {
             const result = await fetch(url);
             const moviesData = await result.json();
             setData(moviesData);
-
+            
             // eslint-disable-next-line array-callback-return
             moviesData.map((item) => {
                 SeriesData.push(item);
-
             })
         } catch (error) {
-            setError(error);
             console.log(error);
         }
-        setLoading(true);
 
     }
 
     useEffect(() => {
-        setLoading(true);
         fetchAPIData(API);
     }, [API]);
 
