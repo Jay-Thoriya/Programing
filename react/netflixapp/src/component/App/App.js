8e61f8d86d7dc6c8c41798a49d0af949d4bf1d95
Jay-Thoriya <120302342+Jay-Thoriya@users.noreply.github.com>
2023-05-16 16:53:36 +0530

    import all function like (Toggle , search) using useReducer

--- react/netflixapp/src/component/App/App.js
+++ react/netflixapp/src/component/App/App.js
@@ -1,44 +1,78 @@
-import Card from "./card/cards"
 import "../../index.css";
-import SeriesData from './card/SeriesData';
+import Card from "./card/cards"
 import Toggle from './nev/toggle';
 import Search from './nev/search';
 import './App.css';
-import { createContext, useState } from "react";
+import { createContext, useEffect, useReducer, useState } from "react";
 import CursorEvent from "./CursorEvent";
 import Playlist from "./nev/Playlist";
 import { useApi } from "./card/SeriesData";
 
 const SeriesDataId = createContext();
+const DispatchValue = createContext();
 function App() {
+  const { moviesData } = useApi('movies');
+  const [series, setSeries] = useState(moviesData);
+  const [isPLaylist, setISPlaylist] = useState(series);
   const [PlaylistSeriesIds, setPlaylistSeriesIds] = useState([]);
   const disk = { PlaylistSeriesIds: PlaylistSeriesIds, setPlaylistSeriesIds: setPlaylistSeriesIds };
-  const [movieData , setMovieData] = useState([]);
-  const { moviesData } = useApi('movies',setMovieData);
-  //console.log("moviesData" , moviesData);
-  
-  const [series, setSeries] = useState(movieData);
-  const [isPLaylist, setISPlaylist] = useState(series);
+  const reducer = (state, action) => {
+    switch (action.type) {
+      case "SEARCH_SERIES":
+        let search = action.payload
+        const result = moviesData.filter(movieInfo =>
+          movieInfo.Title.toLowerCase().includes(search)
+        )
+        setSeries(result);
+        setISPlaylist(result);
+        break;
 
+      case "TOGGLE_CHANGE":
+        const cardContainer = document.querySelector('.card-container');
+        if (cardContainer.style.backgroundColor === "black") {
+          cardContainer.style.backgroundColor = "#77b5fe";
+          document.body.style.backgroundColor = "black";
+          document.querySelector(".cards").style.color = "white";
+        }
+        else {
+          cardContainer.style.backgroundColor = "black";
+          document.body.style.backgroundColor = "#77b5fe";
+          document.querySelector(".cards").style.color = "black";
+        }
+        break;
+
+      default:
+        break;
+    }
+    return state
+  }
+  const [state, dispatch] = useReducer(reducer, moviesData);
+  useEffect(() => {
+    setSeries(moviesData);
+  }, [moviesData])
+  useEffect(() => {
+    setISPlaylist(series);
+  }, [series])
   return (
+    //console.log("moviesData" , moviesData);
     <CursorEvent >
-      <div className='cards'>List Of Top Best Netflix Series 2022
-        <Search setSeries={setSeries} setISPlaylist={setISPlaylist} movieData={movieData}/>
-        <Toggle />
-
-      </div>
-      <Playlist setISPlaylist={setISPlaylist} PlaylistSeriesIds={PlaylistSeriesIds} />
-      <SeriesDataId.Provider value={disk}>
-        <div className="card-container">
-          {isPLaylist.map((value, index) => <Card key={value.id}  {...value} moviesData={moviesData} setMovieData={setMovieData}
-          />
-          )}
+      <DispatchValue.Provider value={dispatch}>
+        <div className='cards'>List Of Top Best Netflix Series 2022
+          <Search setSeries={setSeries} setISPlaylist={setISPlaylist} moviesData={moviesData} />
+          <Toggle />
         </div>
-
-      </SeriesDataId.Provider>
+        <Playlist setISPlaylist={setISPlaylist} PlaylistSeriesIds={PlaylistSeriesIds} moviesData={moviesData} />
+        <SeriesDataId.Provider value={disk}>
+          <div className="card-container">
+            {isPLaylist.map((value, index) => <Card key={value.id}  {...value}
+            />
+            )}
+          </div>
+        </SeriesDataId.Provider>
+      </DispatchValue.Provider>
     </CursorEvent>
   );
 }
 
 export default App;
-export { SeriesDataId };
+export { SeriesDataId, DispatchValue };
